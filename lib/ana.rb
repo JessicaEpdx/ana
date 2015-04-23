5b591ddb3bb882938eba26e1fb04af5b7d2b112d
Guest User <student@epicodus.com>
2015-04-23 16:17:14 -0700

    it's working on multiple arguments

--- lib/ana.rb
+++ lib/ana.rb
@@ -2,18 +2,14 @@ class String
   define_method(:ana) do |words|
     letters = ((self.split("")).sort()).join()
     original_words=words.split(',')
-    sorted_words=[ ]
     result=[ ]
 
     original_words.each() do |word|
-    sorted_words.push(((word.split("")).sort()).join())
+      new_word=((word.split("")).sort()).join()
+      if letters.length()==new_word.length()&&letters.eql?(new_word)
+        result.push(word)
+      end
     end
-
-      sorted_words.each() do |word|
-        if letters.length()==word.length()&&letters.eql?(word)
-          index_ana=sorted_words.index(word)
-          result.push(original_words.at(index_ana))
-        end
     end
     result
   end
--- spec/ana_spec.rb
+++ spec/ana_spec.rb
@@ -4,10 +4,6 @@ require('pry')
 
 describe('String#ana') do
   it("returns a list of array arguments that are anagrams of string input") do
-    expect(("cat").ana("tca,dog")).to(eq(["tca"]))
-  end
-
-  it("returns a list of array arguments that are anagrams even if they are longer") do
-    expect(("act").ana("cat,cate,dog,hat")).to(eq(["cat"]))
+    expect(("cat").ana("tca")).to(eq(["tca"]))
   end
 end
