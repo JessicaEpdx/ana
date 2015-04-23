262856b3d4bd4065a5aa0caed11f6efc59d57409
Guest User <student@epicodus.com>
2015-04-23 15:53:49 -0700

    returns correct answer, but only one

--- lib/ana.rb
+++ lib/ana.rb
@@ -3,11 +3,18 @@ class String
     letters = ((self.split("")).sort()).join()
     original_words=words.split(',')
     sorted_words=[ ]
+    result=[ ]
 
     original_words.each() do |word|
-      sorted_words.push(((word.split("")).sort()).join())
-
+    sorted_words.push(((word.split("")).sort()).join())
     end
-    sorted_words
+
+      sorted_words.each() do |word|
+        if letters.length()==word.length()&&letters.eql?(word)
+          index_ana=sorted_words.index(word)
+          result.push(original_words.at(index_ana))
+        end
+    end
+    result
   end
 end
--- spec/ana_spec.rb
+++ spec/ana_spec.rb
@@ -1,8 +1,13 @@
 require('rspec')
 require('ana')
+require('pry')
 
 describe('String#ana') do
   it("returns a list of array arguments that are anagrams of string input") do
-    expect(("cat").ana(["act", "dog"])).to(eq(["act"]))
+    expect(("cat").ana("tca,dog")).to(eq(["tca"]))
+  end
+
+  it("returns a list of array arguments that are anagrams even if they are longer") do
+    expect(("act").ana("cat,cate,dog,hat")).to(eq(["cat"]))
   end
 end
